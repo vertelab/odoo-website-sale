86a0a594f73515fc8d16f45851bf0d9313eaf866
Haojun Zou <apollo_zhj@msn.com>
2017-03-21 14:43:33 +0100

    website_sale_product_gallery: select variant when linked to variant

--- website_sale_product_gallery/static/src/js/gallery.js
+++ website_sale_product_gallery/static/src/js/gallery.js
@@ -1,7 +1,3 @@
 $(document).ready(function(){
-    $('.oe_website_sale').each(function () {
-        var oe_website_sale = this;
-        $(oe_website_sale).on('change', 'input.js_variant_change, select.js_variant_change, ul[data-attribute_value_ids]', function (ev) {
-        });
-    });
+
 });
