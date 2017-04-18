f71c46e7f4b7570200808a19ba6193eaab487ccf
Robin Chatfield <robin.chatfield@vertel.se>
2017-04-18 08:11:34 +0200

    website_sale_facets_menu: Module created.

---
+++ website_sale_facets_menu/sale.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution, third party addon
+#    Copyright (C) 2004-2017 Vertel AB (<http://vertel.se>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+from openerp import models, fields, api, _
+from openerp import http
+from openerp.http import request
+from openerp.addons.website_sale.controllers.main import website_sale, QueryURL, table_compute
+import logging
+_logger = logging.getLogger(__name__)
+
+#~ class website_sale(website_sale):
+    
+    #~ @http.route([
+        #~ '/shop/variant/<model("product.product"):variant>'
+    #~ ], type='http', auth="public", website=True)
+    #~ def dn_product_variant(self, variant, category='', search='', **kwargs):
+        #~ cr, uid, context, pool = request.cr, request.uid, request.context, request.registry
+        #~ category_obj = pool['product.public.category']
+        #~ template_obj = pool['product.template']
+
+        #~ context.update(active_id=variant.product_tmpl_id.id)
+
+        #~ if category:
+            #~ category = category_obj.browse(cr, uid, int(category), context=context)
+            #~ category = category if category.exists() else False
+
+        #~ attrib_list = request.httprequest.args.getlist('attrib')
+        #~ attrib_values = [map(int,v.split("-")) for v in attrib_list if v]
+        #~ attrib_set = set([v[1] for v in attrib_values])
+
+        #~ keep = QueryURL('/shop', category=category and category.id, search=search, attrib=attrib_list)
+
+        #~ category_ids = category_obj.search(cr, uid, [], context=context)
+        #~ category_list = category_obj.name_get(cr, uid, category_ids, context=context)
+        #~ category_list = sorted(category_list, key=lambda category: category[1])
+
+        #~ pricelist = self.get_pricelist()
+
+        #~ from_currency = pool.get('product.price.type')._get_field_currency(cr, uid, 'list_price', context)
+        #~ to_currency = pricelist.currency_id
+        #~ compute_currency = lambda price: pool['res.currency']._compute(cr, uid, from_currency, to_currency, price, context=context)
+
+        #~ if not context.get('pricelist'):
+            #~ context['pricelist'] = int(self.get_pricelist())
+            #~ product = template_obj.browse(cr, uid, variant.product_tmpl_id.id, context=context)
+
+        #~ values = {
+            #~ 'search': search,
+            #~ 'category': category,
+            #~ 'pricelist': pricelist,
+            #~ 'attrib_values': attrib_values,
+            #~ 'compute_currency': compute_currency,
+            #~ 'attrib_set': attrib_set,
+            #~ 'keep': keep,
+            #~ 'category_list': category_list,
+            #~ 'main_object': product,
+            #~ 'product': product,
+            #~ 'product_product': variant,
+            #~ 'get_attribute_value_ids': self.get_attribute_value_ids
+        #~ }
+        #~ return request.website.render("website_sale.product", values)
