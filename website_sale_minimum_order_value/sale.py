32dd0f08b3ed21a85eaa3cf75aa8f35c64485daa
Anders Wallenquist <anders.wallenquist@vertel.se>
2017-10-24 17:02:32 +0200

    website_sale_minimum_order_value: new module

---
+++ website_sale_minimum_order_value/sale.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution, third party addon
+#    Copyright (C) 2017- Vertel AB (<http://vertel.se>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+from openerp import models, fields, api, _
+
+import openerp
+from openerp import http
+from openerp.http import request
+
+import logging
+_logger = logging.getLogger(__name__)
+
+class sale_order_minvalue(models.Model):
+    _name="sale.order.minvalue"
+    
+    name = fields.Char()
+    destination_ids = fields.Many2many(comodel_name='res.country',string="Destinations")
+    min_value = fields.Float(string="Minimum Order Value")
+    info_text = fields.Text(translation=True)
+
+
+class website_sale(http.Controller):
+
+    @http.route(['/shop/order/note'], type='json', auth="public", website=True)
+    def order_note(self, note, **post):
+        order = request.website.sudo().sale_get_order()
+        if order:
+            order.sudo().note = note
+
