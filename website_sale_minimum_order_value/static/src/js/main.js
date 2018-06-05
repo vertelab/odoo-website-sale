b096908a4be4a149833520029834b87190d4527e
Haojun Zou <apollo_zhj@msn.com>
2018-06-05 14:39:08 +0200

    website_sale_minimum_order_value: change checkout button attribute according to minimum order value. working in progress

--- website_sale_minimum_order_value/static/src/js/main.js
+++ website_sale_minimum_order_value/static/src/js/main.js
@@ -4,4 +4,20 @@ $(document).ready(function () {
             'note': $(this).val(),
         });
     });
+    console.log($("tr#order_total_untaxed").find("span.oe_currency_value").text());
+    $('.oe_website_sale').each(function () {
+        var oe_website_sale = this;
+        $(oe_website_sale).on('click', 'a.js_add_cart_json', function (ev) {
+        }).done(function() {
+            $.ajax({
+                url: '/shop/allowed_order',
+                type: 'post',
+                data: {'order_id': $("a#process_checkout").data("order")},
+                success: function(data){
+                    console.log(data);
+                    //~ $("#top_menu").load(location.href + " #top_menu");
+                }
+            });
+        });
+    });
 });
