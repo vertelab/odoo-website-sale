4597c5399475569e9766a9c458273952e73cfb0a
Haojun Zou <apollo_zhj@msn.com>
2018-11-23 14:35:33 +0100

    website_sale_one_click_shopping: fix top_menu when shopping cart is clicked

--- website_sale_one_click_shopping/static/src/js/main.js
+++ website_sale_one_click_shopping/static/src/js/main.js
@@ -7,7 +7,14 @@ $(document).ready(function () {
             data: $(this).closest('form').serialize(),
             success: function(){
                 $("#top_menu").load(location.href + " #top_menu");
+                $("#top_menu").css({'position': 'fixed', 'top': '0px'});
             }
         });
     });
+});
+
+$(window).scroll(function() {
+    if ($(this).scrollTop() == 0) {
+        $("#top_menu").css({'position': '', 'top': ''});
+    }
 });
