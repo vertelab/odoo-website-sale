a6e9043c636b85d467d46545d487d51a3f351191
sandra <sandra.pettersson@vertel.se>
2020-08-24 09:50:29 +0200

    website_portal_1028: changed campaign lang

--- website_portal_1028/controllers/main.py
+++ website_portal_1028/controllers/main.py
@@ -82,6 +82,10 @@ class website_account(http.Controller):
                 phase = helper.campaign_id.phase_ids.filtered(lambda p: p.reseller_pricelist)[0]
                 phase_date = helper.campaign_id.phase_ids.filtered(lambda p: not p.reseller_pricelist)[0]
                 price = variant.pricelist_chart_ids.filtered(lambda c: c.pricelist_chart_id.pricelist == phase.pricelist_id).rec_price
+                products = request.env['product.product'].sudo().search([('website_published','=',True), ('sale_ok','=',True), ('active','=',True)])
+                partner = request.env.user.partner_id.commercial_partner_id
+                pricelist = partner.property_product_pricelist
+
             date_start = phase_date.start_date
             date_stop = phase_date.end_date
             if not date_stop:
@@ -93,14 +97,17 @@ class website_account(http.Controller):
             # Calculate customers price at campaign start
             if not price or price < 1:
                 line['price'] = _(' ')
+
             else:
                 line['price'] = '%s %s' % (lang.format(
                             '%f',
-                            price,
+                            product.get_pricelist_chart_line(pricelist).rec_price,
                             grouping=True, monetary=True, context=request.env.context)[:-4],
-                            phase.pricelist_id.currency_id.name
+                            pricelist.currency_id.name
                     )
         return res
+
+
 
     def _prepare_portal_layout_values(self):
         """ prepare the values to render portal layout """
