aaf089228f626e1420852d01f36da3328e32115e
sandra <sandra.pettersson@vertel.se>
2020-08-24 10:45:39 +0200

    website_portal_1028: simplify code

--- website_portal_1028/controllers/main.py
+++ website_portal_1028/controllers/main.py
@@ -80,13 +80,12 @@ class website_account(http.Controller):
                 # Campaign aimed at salons
                 phase = helper.campaign_phase_id
                 phase_date = helper.campaign_phase_id
-                price = variant.pricelist_chart_ids.filtered(lambda c: (c.pricelist_chart_id.pricelist == phase.pricelist_id)).rec_price   
             else:
                 # Campaign aimed at consumers
                 phase = helper.campaign_id.phase_ids.filtered(lambda p: p.reseller_pricelist)[0]
                 phase_date = helper.campaign_id.phase_ids.filtered(lambda p: not p.reseller_pricelist)[0]
-                price = variant.pricelist_chart_ids.filtered(lambda c: c.pricelist_chart_id.pricelist == phase.pricelist_id).rec_price
-                
+
+            price = product.get_pricelist_chart_line(pricelist).rec_price 
             date_start = phase_date.start_date
             date_stop = phase_date.end_date
             if not date_stop:
@@ -102,12 +101,11 @@ class website_account(http.Controller):
             else:
                 line['price'] = '%s %s' % (lang.format(
                             '%f',
-                            product.get_pricelist_chart_line(pricelist).rec_price,
+                            price,
                             grouping=True, monetary=True, context=request.env.context)[:-4],
                             pricelist.currency_id.name
                     )
         return res
-
 
 
     def _prepare_portal_layout_values(self):
