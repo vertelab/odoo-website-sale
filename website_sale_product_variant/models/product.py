dd37684035af6bf620d0f5cb0fe973ccb4859158
victor ekstrom <victor.ekstrom@vertel.se>
2021-08-06 12:22:07 +0000

    website_sale_product_variant: migrated to odoo14

--- website_sale_product_variant/models/product.py
+++ website_sale_product_variant/models/product.py
@@ -1,34 +1,15 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution, third party addon
-#    Copyright (C) 2004-2017 Vertel AB (<http://vertel.se>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-from openerp import models, fields, api, _
-from openerp import http
-from openerp.http import request
-from openerp.addons.website_sale.controllers.main import website_sale, QueryURL, table_compute
 
-from timeit import default_timer as timer
+import logging
+from odoo import _, api, fields, models
+from odoo.osv import expression
+from odoo.exceptions import ValidationError
 
-class product_template(models.Model):
+_logger = logging.getLogger(__name__)
+
+class ProductTemplate(models.Model):
     _inherit = "product.template"
 
-    @api.multi
     def get_default_variant(self):
         self.ensure_one()
         if not self.product_variant_ids:
@@ -37,58 +18,14 @@ class product_template(models.Model):
             return self.product_variant_ids.filtered(lambda v: v.default_variant == True) or (self.product_variant_ids[0] if self and len(self.product_variant_ids) > 0 else None)
         return None
 
-class product_product(models.Model):
+class ProductProduct(models.Model):
     _inherit = 'product.product'
 
     default_variant = fields.Boolean(string='Default Variant')
-
-class website_sale(website_sale):
-    @http.route([
-        '/shop/variant/<model("product.product"):variant>'
-    ], type='http', auth="public", website=True)
-    def shop_product_variant(self, variant, category='', search='', **kwargs):
-        cr, uid, context, pool = request.cr, request.uid, request.context, request.registry
-        category_obj = pool['product.public.category']
-        template_obj = pool['product.template']
-
-        context.update(active_id=variant.product_tmpl_id.id)
-
-        if category:
-            category = category_obj.browse(cr, uid, int(category), context=context)
-            category = category if category.exists() else False
-
-        attrib_list = request.httprequest.args.getlist('attrib')
-        attrib_values = [map(int,v.split("-")) for v in attrib_list if v]
-        attrib_set = set([v[1] for v in attrib_values])
-
-        keep = QueryURL('/shop', category=category and category.id, search=search, attrib=attrib_list)
-
-        category_ids = category_obj.search(cr, uid, [], context=context)
-        category_list = category_obj.name_get(cr, uid, category_ids, context=context)
-        category_list = sorted(category_list, key=lambda category: category[1])
-
-        pricelist = self.get_pricelist()
-
-        from_currency = pool.get('product.price.type')._get_field_currency(cr, uid, 'list_price', context)
-        to_currency = pricelist.currency_id
-        compute_currency = lambda price: pool['res.currency']._compute(cr, uid, from_currency, to_currency, price, context=context)
-
-        if not context.get('pricelist'):
-            context['pricelist'] = int(self.get_pricelist())
-            product = template_obj.browse(cr, uid, variant.product_tmpl_id.id, context=context)
-
-        values = {
-            'search': search,
-            'category': category,
-            'pricelist': pricelist,
-            'attrib_values': attrib_values,
-            'compute_currency': compute_currency,
-            'attrib_set': attrib_set,
-            'keep': keep,
-            'category_list': category_list,
-            'main_object': product,
-            'product': product,
-            'product_product': variant,
-            'get_attribute_value_ids': self.get_attribute_value_ids
-        }
-        return request.website.render("website_sale.product", values)
+    
+    @api.constrains('default_variant')
+    def _constrain_default_variant(self):
+        for variant in self:
+            defaults = variant.product_tmpl_id.product_variant_ids.filtered('default_variant')
+            if len(defaults) > 1:
+                raise ValidationError(_("A template can not have more than one default variant."))
