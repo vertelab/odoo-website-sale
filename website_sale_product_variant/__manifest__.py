386baff1e0bb445d4e27a0d8b6e6f801f4e533f4
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2023-06-19 11:29:06 +0200

    Update __manifest__.py
    minor spelling error

--- website_sale_product_variant/__manifest__.py
+++ website_sale_product_variant/__manifest__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 
 {
-    'name': 'Website Sale: Website Sale Product Variant',
+    'name': 'Website Sale: Product Variant',
     'version': '14.0.0.0.0',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'Use product variant in webshop.',
