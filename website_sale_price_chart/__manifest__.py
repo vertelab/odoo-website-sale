982882390c3043998fe7a2b092c715eed08933d3
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2023-06-19 11:28:20 +0200

    Update __manifest__.py
    minor spelling error

--- website_sale_price_chart/__manifest__.py
+++ website_sale_price_chart/__manifest__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 
 {
-    'name': 'Website Sale: Website Sale Price Chart',
+    'name': 'Website Sale: Price Chart',
     'version': '14.0.0.0.0',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'Adds price types to products.',
