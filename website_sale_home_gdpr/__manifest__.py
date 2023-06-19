9124e136dadf31449860261d59b5be6740bef2a9
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2023-06-19 11:10:48 +0200

    Update __manifest__.py
    Minor spelling issue

--- website_sale_home_gdpr/__manifest__.py
+++ website_sale_home_gdpr/__manifest__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 
 {
-    'name': 'Website Sale: Website Sale Home GDPR',
+    'name': 'Website Sale: Home GDPR',
     'version': '14.0.0.0.0',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'Form for claims.',
